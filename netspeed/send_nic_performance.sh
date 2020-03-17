7ce503d3a44705c250b60ae1d374c29ff54af377
root <flyhi.gao@gmail.com>
2020-03-17 17:29:14 +0800

    add par

--- netspeed/send_nic_performance.sh
+++ netspeed/send_nic_performance.sh
@@ -1,8 +1,11 @@
 echo "sender_nic is $1"
 ethtool -C $1 adaptive-rx off
+ethtool -C $1 adaptive-tx off
 ethtool -K $1 tso off
 ethtool -K $1 gro off
 ethtool -K $1 lro off
 ethtool -K $1 gso off
 ethtool -K $1 rx off
 ethtool -K $1 tx off
+ethtool -A $1 autoneg off
+ethtool -r $1
