d227a18b27c388be8d1d68b6746bade0cc4c8084
flyhigao <47943621+flyhigao@users.noreply.github.com>
2020-09-03 15:11:04 +0800

    Update disablenic.sh

--- netspeed/disablenic.sh
+++ netspeed/disablenic.sh
@@ -2,8 +2,9 @@
 IFACE=$1
 for i in rx tx sg tso ufo gso gro lro rxvlan txvlan nocache copy sg rxhash; do ethtool -K $IFACE $i off; done 
 ethtool -L $IFACE combined 1
+ethtool -G $IFACE rx 4096 tx 4096
 ifconfig $IFACE mtu 9000
 systemctl stop irqbalance
-systemctl disable irqbalance
+pkill irqbalance 
 
 
