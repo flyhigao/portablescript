21783f6998f3a4fa76aeca44c4dceb8fe8730e09
flyhigao <47943621+flyhigao@users.noreply.github.com>
2022-10-04 00:01:35 +0800

    Create datalimit.sh
    在aws设备上通过定期检测网卡tx流量,进行限流

---
+++ netspeed/datalimit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#T1b=`cat /sys/class/net/eth0/statistics/tx_bytes`
+sum=`cat /sys/class/net/*/statistics/tx_bytes|awk '{sum+=$1} END {print sum}'`
+echo $sum
+#1G = 1073741824 byte
+if [ $sum -gt 98784247808 ];then
+        echo `date` all nics total data transferd are $sum , larger than 92g, stop tcp/udp 443 port | tee -a /root/ethdatalimit.log
+        iptables -F
+        iptables -A INPUT -p tcp --destination-port 443 -j DROP
+        iptables -A INPUT -p udp --destination-port 443 -j DROP
+else
+        echo $sum less than 92g,go on
+fi
